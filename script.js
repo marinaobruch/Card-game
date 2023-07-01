93b4d84e42167f61f20cd28816ad7394b719b2b3
«Marina» <marina_obruch@mail.ru>
2023-07-01 20:46:27 +0500

    Реализована отправка на стр путем выбора уровня

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const renderGame = () => {
     const renderLevelPage = `
         <form class="common__block center">
         <h1 class="common__block_text">Выбери сложность</h1>
-        <div class="common__block_options">
+        <div class="common__block_options options-form">
 
             <label for="1">
                 <div class="common__block_item common__block_item-first">
@@ -29,10 +29,31 @@ const renderGame = () => {
             </label>
 
         </div>
-        <button type="submit" class="common__block_options-button">Старт</button>
+        <button type="submit" class="common__block_options-button start-button">Старт</button>
         </form>
         `
+
     levelPage.innerHTML = renderLevelPage;
+
+    const levelForm = document.querySelector(".options-form");
+    const startButton = document.querySelector(".start-button");
+
+    function choiseLevel() {
+        startButton.addEventListener('click', () => {
+            const level = levelForm.querySelector('input[type="radio"]:checked').value;
+
+            if (level === 1) {
+                levelPage.innerHTML = `level 1`;
+            } else if (level === 2) {
+                levelPage.innerHTML = `level 2`;
+            } else {
+                levelPage.innerHTML = `level 3`;
+            }
+
+        })
+    }
+    choiseLevel();
+
 }
 
 renderGame();
